964bf2d290c0eaadd8d844ac555693c7e9c6b758
kdavis0221 <51808027+kdavis0221@users.noreply.github.com>
2019-07-20 18:51:55 -0400

    Update userinfo.py

--- userinfo.py
+++ userinfo.py
@@ -11,7 +11,7 @@ class User:
     def save(self):
         # finds the 'users' directory and looks to open the associated file name
         current_dir = os.getcwd() + "/users"
-        file_to_open = os.path.join(current_dir, self.name + ".dat")
+        file_to_open = os.path.join(current_dir, self.filename)
         user_file = open(file_to_open, "wb")
         pickle.dump(self, user_file)
         user_file.close()
@@ -20,4 +20,9 @@ class User:
         tag = tag + ".dat"
         return pickle.load(open(tag, "rb"))
 
+    def show_score(self):
+        return self.score
+
+    def add_score(self, num):
+        self.score += num
 
